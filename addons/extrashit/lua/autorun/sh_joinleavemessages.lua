33d4cfa0ae5a61abc5ac398eafa3329ee0226ce5
Jck123 <jck.1.2.3.a@gmail.com>
2020-07-11 13:47:41 -0400

    disconnect messages are functioning

--- addons/extrashit/lua/autorun/sh_joinleavemessages.lua
+++ addons/extrashit/lua/autorun/sh_joinleavemessages.lua
@@ -1,8 +1,9 @@
 if SERVER then
-    util.AddNetworkString("plyName")
+    util.AddNetworkString("plyJoin")
+    util.AddNetworkString("plyLeave")
 
     hook.Add("PlayerInitialSpawn", "jckjoin", function(ply)
-    net.Start("plyName")
+    net.Start("plyJoin")
         if ply:IsAdmin() then
             net.WriteString(ply:Nick() .. " (Staff Member)")
         else
@@ -11,8 +12,29 @@ if SERVER then
         net.Broadcast()
     end)
 
+    gameevent.Listen("player_disconnect")
+    hook.Add("player_disconnect", "jckleave", function(data)
+
+        local tblReasons =
+        {
+            ["Disconnect by user."]         = " has left the server",
+            ["Too many warnings"]           = " reached the warning limit!",
+            ["Connection closing"]          = " has lost connection",
+            ["timed out"]                   = " has crashed out of the server",
+            ["Client left game"]            = " left because of a Steam mishap.",
+            ["[ULX] Kicked from server"]    = " has been kicked from the server",
+            ["_default"]                    = " has left the server",
+        }
+        net.Start("plyLeave")
+            net.WriteString(data.name)
+            net.WriteString(tblReasons[data.reason])
+        net.Broadcast()
+    end)
 else
-    net.Receive("plyName", function()
+    net.Receive("plyJoin", function()
         chat.AddText(Color(255, 255, 255), "Player ", Color(255, 0, 0), net.ReadString(), Color(255, 255, 255), " has joined the game")
+    end)
+    net.Receive("plyLeave", function()
+        chat.AddText(Color(255, 255, 255), "Player ", Color(255, 0, 0), net.ReadString(), Color(255, 255, 255), net.ReadString())
     end)
 end
