65ff71dab8e93f291ae66291aebc3350ab6d820e
Jck123 <jck.1.2.3.a@gmail.com>
2020-07-17 00:17:42 -0400

    trigger class v1.0

--- addons/extrashit/lua/autorun/server/sv_triggers.lua
+++ addons/extrashit/lua/autorun/server/sv_triggers.lua
@@ -1,9 +1,20 @@
-hook.Add("EntityTakeDamage", "Spawn_Trigger", function(ent, dmgInfo)
-    if dmgInfo:IsFallDamage() then return true end
-    if !dmgInfo:GetAttacker():IsPlayer() or !ent:IsPlayer() then return end
-    local vicPos = ent:GetPos()
-    if vicPos.x > 639.968750 and vicPos.x < 1023.968750 and vicPos.y > -895.968750 and vicPos.y < 800.031250 and vicPos.z > -144 and vicPos.z < 64.031250 then
-        dmgInfo:GetAttacker():SendLua("notification.AddLegacy('Fuck off', NOTIFY_ERROR, 15)")
-        return true
-    end
-end)
+trigger = {}
+
+function trigger.Create(startVec, endVec, name)
+
+    if startVec.x > endVec.x or startVec.y > endVec.y or startVec.z > startVec.z then return error("[ERROR] All startVec values MUST be smaller than endVec values!") end
+
+    hook.Add("EntityTakeDamage", "trigger_" .. name, function(ent, dmgInfo)
+        if dmgInfo:IsFallDamage() then return true end
+        if !dmgInfo:GetAttacker():IsPlayer() or !ent:IsPlayer() then return end
+        local vicPos = ent:GetPos()
+        if vicPos.x > startVec.x and vicPos.x < endVec.x and vicPos.y > startVec.y and vicPos.y < endVec.y and vicPos.z > startVec.z and vicPos.z < endVec.z then
+            dmgInfo:GetAttacker():SendLua("notification.AddLegacy('Fuck off', NOTIFY_ERROR, 15)")
+            return true
+        end
+    end)
+end
+
+function trigger.Remove(name)
+    hook.Remove("EntityTakeDamage", "trigger_" .. name)
+end
