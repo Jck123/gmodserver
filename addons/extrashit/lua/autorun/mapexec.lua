0385e56fc26a8db40d6e25d063fe5fbde6c44d72
Jck123 <jck.1.2.3.a@gmail.com>
2021-01-04 00:50:02 -0500

    fixed string

--- addons/extrashit/lua/autorun/mapexec.lua
+++ addons/extrashit/lua/autorun/mapexec.lua
@@ -1,5 +1,5 @@
 --//This file runs code depending on the map that is currently running//--
 
 hook.Add("Initialize", "mapexec", function() 
-    require("../mapexec" .. game.GetMap() .. ".lua")
+    require("../mapexec/" .. game.GetMap() .. ".lua")
 end)
